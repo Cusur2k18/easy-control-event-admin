0820005577ba24bafe476c588bcae4597267c120
Christofer Flores <cristofer.flort@gmail.com>
2018-09-27 22:30:46 -0500

    Fix an issue when display the time and add the grid view on events

--- src/components/UI/CardComponent/CardComponent.jsx
+++ src/components/UI/CardComponent/CardComponent.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import * as moment from 'moment';
+import { withRouter } from 'react-router-dom';
 
-export default (props) => {
+export default withRouter(props => {
 
-  let content = [];
-
-  for (let index = 0; index < props.elements; index++) {
-    content.push((
-      <div className="col-md-3 mt-5" key={index}>
+  return props.elements.map( ev => {
+    return (
+      <div className="col-md-3 mt-5" key={ev.id}>
         <div className="card" >
-          <img className="card-img-top" src="http://via.placeholder.com/150x150" alt="Card cap" />
-          <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <img className="card-img-top" src={ev.coverImg ? ev.coverImg : 'http://via.placeholder.com/150x150' } alt={ev.name} />
+          <p className="card-text-title p-4">{ev.name}</p>
+          <div className="card-body border border-dark p-2 mx-4 mb-4">
+            <p className="card-text"> <b>Fecha de inicio: </b>{moment(ev.startDateTime).format('dddd d [de] MMMM [de] YYYY [a las] hh:mm a')}</p>
+            <p className="card-text"> <b>Fecha de finalizacion: </b>{moment(ev.endDateTime).format('dddd d [de] MMMM [de] YYYY [a las] hh:mm a')}</p>
+          </div>
+          <div className="card-footer">
+            <a 
+              className="link text-underline font-italic item-clickable"
+              onClick={() => {props.history.push('/events/' + ev.uuid)}}>
+              Ver mas
+            </a>
           </div>
         </div>
       </div>
-    ))
-  }
-
-  return content
-    
-}
+    )
+  })
+})
--- src/utils/displayTime.js
+++ src/utils/displayTime.js
@@ -4,7 +4,7 @@ import * as moment from 'moment'
 export function displayTime(starDate, endDate) {
   return (
     <React.Fragment>
-      <i className="date-table-element">{moment(starDate).format('dd DD MMM h:mm a')}</i> - <i className="date-table-element">{moment(endDate).format('dd DD MMM h:mm a')}</i>
+      <i className="date-table-element">{moment(starDate).format('dd d MMM h:mm a')}</i> - <i className="date-table-element">{moment(endDate).format('dd d MMM h:mm a')}</i>
     </React.Fragment>
   )
 }
